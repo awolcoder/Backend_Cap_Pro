7cb8799741fde56b04c5b64cb9d0b4a006b01240
awolcoder <abdulbasitawil42@gmail.com>
2025-10-15 19:36:40 +0300

    feat: add 'tags' app with Tag model, serializers, and admin permissions

---
+++ tags/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from .models import Tag
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+queryset = Tag.objects.all()
+serializer_class = TagSerializer
+
+
+def get_permissions(self):
+if self.action in ['list', 'retrieve']:
+return [AllowAny()]
+return [IsAdminUser()]
