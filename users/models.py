16d0f55de693fd4fc6ed47124890653fbd525c5d
awolcoder <abdulbasitawil42@gmail.com>
2025-10-15 19:35:37 +0300

    feat: add 'users' app with custom User model, serializers, views, and JWT registration/login endpoints

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+# Extend later if needed
+bio = models.TextField(blank=True, null=True)
+
+
+def __str__(self):
+return self.username
---
+++ users/views.py
@@ -0,0 +1,34 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, RegisterSerializer
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.GenericViewSet):
+queryset = User.objects.all()
+serializer_class = UserSerializer
+
+
+def get_permissions(self):
+if self.action in ['create', 'register']:
+return [AllowAny()]
+return [IsAuthenticated()]
+
+
+@action(detail=False, methods=['post'], permission_classes=[AllowAny])
+def register(self, request):
+serializer = RegisterSerializer(data=request.data)
+serializer.is_valid(raise_exception=True)
+user = serializer.save()
+return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+
+
+@action(detail=False, methods=['get'])
+def me(self, request):
+serializer = UserSerializer(request.user)
+return Response(serializer.data)
