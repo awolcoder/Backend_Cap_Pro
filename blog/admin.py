bc319d6df9464d98f9feedb76fefda85ecf9b9e1
awolcoder <abdulbasitawil42@gmail.com>
2025-10-15 19:38:40 +0300

    chore: remove old 'blog' app files

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, Category, Tag, User
-
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Tag)
-   
