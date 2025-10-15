c94756910d788603e3b491c49ee1e64366ca59d2
awolcoder <abdulbasitawil42@gmail.com>
2025-10-15 19:35:56 +0300

    feat: add 'categories' app with Category model, serializers, and admin permissions

---
+++ categories/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+queryset = Category.objects.all()
+serializer_class = CategorySerializer
+
+
+def get_permissions(self):
+if self.action in ['list', 'retrieve']:
+return [AllowAny()]
+return [IsAdminUser()]
