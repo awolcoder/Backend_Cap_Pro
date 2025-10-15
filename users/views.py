0f1acdeb62d77ddd3f5725baf29bcf14c8030571
awolcoder <abdulbasitawil42@gmail.com>
2025-10-15 21:59:30 +0300

    👤 feat: add 'users' app with custom User model, serializers, views, and JWT registration/login endpoints

--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer, RegisterSerializer
 
-
 User = get_user_model()
 
 
